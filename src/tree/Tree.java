50ee01ca19c8f29c45d60c19425eed6ff93cf81a
B Kleinen <kleinen@infrastructure.de>
2014-06-05 13:15:45 +0200

    Tree and AVL

---
+++ src/tree/Tree.java
@@ -0,0 +1,453 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Simple Binary Search Tree Implementation. Algorithms are adaptations of those
+ * in Cormen, Leiserson, and Rivest's <em>Introduction to Algorithms.
+ * 
+ * @author kleinen
+ * 
+ * @param <K>
+ * @param <V>
+ */
+public class Tree<K extends Comparable<K>, V> implements Iterable<K> {
+	private static final String DELIM = ", ";
+	protected Node root;
+
+	/**
+	 * 
+	 * Entry/Node in the Tree. Could be extended to implement Map.Entry<K,V>
+	 * 
+	 * @see java.util.Map.Entry
+	 */
+	public class Node {
+		K key;
+		V value;
+		Node parent;
+		Node leftChild;
+		Node rightChild;
+
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public String toString() {
+			return "(" + key.toString() + ","
+					+ ((leftChild == null) ? "" : leftChild.toString()) + ","
+					+ ((rightChild == null) ? "" : rightChild.toString()) + ")";
+		}
+
+		public String toStringBalanceFactor() {
+			return "("
+					+ key.toString()
+					+ "["
+					+ balanceFactor
+					+ "],"
+					+ ((leftChild == null) ? "" : leftChild
+							.toStringBalanceFactor())
+					+ ","
+					+ ((rightChild == null) ? "" : rightChild
+							.toStringBalanceFactor()) + ")";
+		}
+
+		public int size() {
+			int l = 0, r = 0;
+			if (leftChild != null)
+				l = leftChild.size();
+			if (rightChild != null)
+				r = rightChild.size();
+			return l + 1 + r;
+
+		}
+
+		public int height() {
+			int lh = -1, rh = -1;
+			if (leftChild != null)
+				lh = 1 + leftChild.height();
+			if (rightChild != null)
+				rh = 1 + rightChild.height();
+			return Math.max(lh, rh);
+		}
+
+		int balanceFactor;
+
+		public int computeBalanceFactor() {
+			int lh = -1, rh = -1;
+			if (leftChild != null)
+				lh = 1 + leftChild.height();
+			if (rightChild != null)
+				rh = 1 + rightChild.height();
+			return balanceFactor = lh - rh;
+		}
+
+		public Tree<K, V>.Node min() {
+			if (leftChild == null)
+				return this;
+			else
+				return leftChild.min();
+		}
+
+		public Tree<K, V>.Node max() {
+			if (rightChild == null)
+				return this;
+			else
+				return rightChild.max();
+		}
+	}
+
+	public Node find(K key) {
+		Node currentNode = root;
+		while (currentNode != null) {
+			int c = key.compareTo(currentNode.key);
+			if (c == 0)
+				return currentNode;
+			else {
+				if (c < 0)
+					currentNode = currentNode.leftChild;
+				else
+					currentNode = currentNode.rightChild;
+			}
+		}
+		// didn't find it
+		return null;
+	}
+
+	public Node findRecursive(K key) throws Exception {
+		return findRecursive(key, root);
+	}
+
+	public Node findRecursive(K key, Node currentNode) {
+		if (currentNode == null)
+			return null;
+		int c = key.compareTo(currentNode.key);
+		if (c == 0)
+			return currentNode;
+		else {
+			if (c < 0)
+				return findRecursive(key, root.leftChild);
+			else
+				return findRecursive(key, root.rightChild);
+		}
+	}
+
+	public Node insert(K key) {
+		return insert(key, null);
+	}
+
+	public void insert(K[] keys) {
+		for (K key : keys) {
+			insert(key);
+		}
+	}
+
+	public Node insert(K key, V value) {
+		return insert(new Node(key, value));
+	}
+
+	public Node insert(Node node) {
+		if (root == null) {
+			root = node;
+			return node;
+		}
+		Node current = root, parent = null;
+		while (current != null) {
+			parent = current;
+			if (node.key.compareTo(current.key) < 0) {
+				current = current.leftChild;
+				if (current == null) {
+					parent.leftChild = node;
+					node.parent = parent;
+				}
+			} else {
+				current = current.rightChild;
+				if (current == null) {
+					parent.rightChild = node;
+					node.parent = parent;
+				}
+			}
+		}
+		return node;
+	}
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public Node getRoot() {
+		return root;
+	}
+
+	public String toString() {
+		return traverseInOrderToString(root)
+				.replaceAll("(" + DELIM + ")+", DELIM)
+				.replaceAll("^" + DELIM, "").replaceAll(DELIM + "$", "");
+	}
+
+	private String traverseInOrderToString(Node node) {
+		if (node == null)
+			return "";
+		else
+			return traverseInOrderToString(node.leftChild) + DELIM
+					+ node.key.toString() + DELIM
+					+ traverseInOrderToString(node.rightChild);
+	}
+
+	public List<Node> delete(K key) {
+		List<Node> rebalance = new ArrayList<Node>();
+
+		Node node = find(key);
+		if (node == null)
+			return null;
+		if (node.leftChild == null) {
+			if (node.parent == null)
+				root = node.rightChild;
+			else if (node.parent.leftChild == node)
+				node.parent.leftChild = node.rightChild;
+			else
+				node.parent.rightChild = node.rightChild;
+			if (node.rightChild != null)
+				node.rightChild.parent = node.parent;
+			rebalance.add(node.rightChild == null ? node.parent
+					: node.rightChild);
+			return rebalance;
+		}
+		if (node.rightChild == null) {
+			if (node.parent == null)
+				root = node.leftChild;
+			else if (node.parent.leftChild == node)
+				node.parent.leftChild = node.leftChild;
+			else
+				node.parent.rightChild = node.leftChild;
+			if (node.leftChild != null)
+				node.leftChild.parent = node.parent;
+			rebalance.add(node.leftChild);
+			return rebalance;
+		}
+		// node has 2 childs.
+		Node succ = successor(node);
+		if (succ == node.rightChild) {
+			transplant(node, succ);
+			succ.leftChild = node.leftChild;
+			succ.leftChild.parent = succ;
+		} else {
+			if (succ.rightChild == null)
+				rebalance.add(succ.parent);
+			else
+				rebalance.add(succ.rightChild);
+			transplant(succ, succ.rightChild);
+			replace(node, succ);
+		}
+		rebalance.add(succ);
+		return rebalance;
+	}
+
+	public Node successor(K key) {
+		return successor(find(key));
+	}
+
+	public Node successor(Node node) {
+		if (node.rightChild != null)
+			return minimum(node.rightChild);
+		Node up = node.parent;
+		Node succ = node;
+		while ((up != null) && succ == up.rightChild) {
+			succ = up;
+			up = up.parent;
+		}
+		return up;
+	}
+
+	public String toStringTree() {
+		if (root == null)
+			return "()";
+		return toStringTree(root);
+	}
+
+	public String toStringTree(Node node) {
+		String left = "(", right = ")", middle = "-";
+		if ((node.leftChild == null) && (node.rightChild == null)) {
+			left = right = middle = "";
+		}
+		String result = left;
+		if (node.leftChild != null) {
+			result += toStringTree(node.leftChild);
+		}
+		result += middle;
+		result += node.key.toString();
+		result += middle;
+		if (node.rightChild != null) {
+			result += toStringTree(node.rightChild);
+		}
+		result += right;
+		return result;
+	}
+
+	public Node minimum() {
+		return minimum(root);
+	}
+
+	public Node minimum(Node node) {
+		while (node.leftChild != null)
+			node = node.leftChild;
+		return node;
+	}
+
+	/**
+	 * moves subtree starting at b to a's position.
+	 * 
+	 * @param a
+	 * @param b
+	 */
+	public void transplant(Node a, Node b) {
+		if (a.parent == null) {
+			root = b;
+			if (b != null)
+				b.parent = null;
+		} else {
+			if (b != null) {
+				if (b == b.parent.leftChild)
+					b.parent.leftChild = null;
+				else
+					b.parent.rightChild = null;
+
+				b.parent = a.parent;
+			}
+			if (a == a.parent.leftChild)
+				a.parent.leftChild = b;
+			else
+				a.parent.rightChild = b;
+			a.parent = null;
+		}
+
+	}
+
+	/**
+	 * replaces node a with node b.
+	 * 
+	 * @param a
+	 * @param b
+	 */
+	public void replace(Node a, Node b) {
+		if (a.parent == null)
+			root = b;
+		else {
+			if (a.parent.leftChild == a)
+				a.parent.leftChild = b;
+			else
+				a.parent.rightChild = b;
+		}
+		b.parent = a.parent;
+		a.parent = null;
+		b.leftChild = a.leftChild;
+		if (b.leftChild != null)
+			b.leftChild.parent = b;
+		b.leftChild.parent = b;
+		b.rightChild = a.rightChild;
+		if (b.rightChild != null)
+			b.rightChild.parent = b;
+	}
+
+	public int size() {
+		if (root == null)
+			return 0;
+		return root.size();
+	}
+
+	public enum Cursor {
+		LEFT, THIS, RIGHT, DONE
+	};
+
+	public class Iterator implements java.util.Iterator<K> {
+		private Cursor c;
+		private Node node;
+		private Node max;
+
+		public Iterator(Node node) {
+			if (node == null)
+				c = Cursor.RIGHT;
+			else {
+				this.node = node;
+				this.c = Cursor.LEFT;
+				max = node;
+				while (max.rightChild != null)
+					max = max.rightChild;
+			}
+
+		}
+
+		@Override
+		public boolean hasNext() {
+			return !((node == max) && c == Cursor.RIGHT);
+		}
+
+		@Override
+		public K next() {
+			switch (c) {
+			case LEFT:
+				if (node.leftChild == null) {
+					c = Cursor.THIS;
+					return next();
+				} else {
+					node = node.leftChild;
+					return next();
+				}
+			case THIS:
+				c = Cursor.RIGHT;
+				return node.key;
+
+			case RIGHT:
+				if (node.rightChild == null) {
+					c = Cursor.DONE;
+					return next();
+				} else {
+					c = Cursor.LEFT;
+					node = node.rightChild;
+					return next();
+				}
+			case DONE:
+				if (node.parent == null)
+					throw new NoSuchElementException();
+				else {
+					if (node.parent.leftChild == node)
+						c = Cursor.THIS;
+					else
+						c = Cursor.DONE;
+				}
+				node = node.parent;
+				return next();
+			}
+			// should never reach this;
+			throw new NoSuchElementException();
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+
+		}
+
+	}
+
+	@Override
+	public java.util.Iterator<K> iterator() {
+		return this.new Iterator(root);
+	}
+
+	public Node min() {
+		return root.min();
+	}
+
+	public Node max() {
+		return root.max();
+	}
+
+	public Node succ(Tree<K,V>.Node current) {
+		
+		return current.rightChild.min();
+	}
+
+}
---
+++ test/tree/TreeTest.java
@@ -0,0 +1,480 @@
+package tree;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TreeTest {
+	Tree<String, Object> tree;
+
+	@Before
+	public void setUp() throws Exception {
+		tree = new Tree<String, Object>();
+	}
+
+	@Test
+	public void testInsertSimple() {
+		tree.insert("hallo");
+		assertEquals("hallo", tree.getRoot().key);
+	}
+
+	@Test
+	public void testToStringSimple() {
+		tree.insert("a");
+		assertEquals("a", tree.toString());
+	}
+
+	@Test
+	public void testInsertAndToString() {
+		tree.insert("b");
+		tree.insert("c");
+		tree.insert("a");
+
+		assertEquals("a, b, c", tree.toString());
+		Tree<String, Object>.Node a = tree.find("a");
+		Tree<String, Object>.Node b = tree.find("b");
+		Tree<String, Object>.Node c = tree.find("c");
+		assertEquals(b, a.parent);
+		assertEquals(b, c.parent);
+		assertEquals(null, b.parent);
+	}
+
+	@Test
+	public void testMinimum() {
+		tree.insert("b");
+		tree.insert("c");
+		tree.insert("a");
+
+		assertEquals("a", tree.minimum().key);
+	}
+
+	@Test
+	public void testInsertAndToStringInteger() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(11);
+		it.insert(23);
+		it.insert(28);
+		it.insert(1);
+		assertEquals("1, 5, 11, 12, 20, 23, 28", it.toString());
+		assertEquals("((1-5-11)-12-(-20-(-23-28)))", it.toStringTree());
+	}
+
+	@Test
+	public void testMax() {
+		Tree<Integer, Object> tree = new Tree<Integer, Object>();
+		tree.insert(new Integer[] { 10, 7, 3, 9, 20, 15, 11, 13, 30 });
+		assertEquals(30, tree.max().key.intValue());
+	}
+
+	@Test
+	public void testMin() {
+		Tree<Integer, Object> tree = new Tree<Integer, Object>();
+		tree.insert(new Integer[] { 10, 7, 3, 9, 20, 15, 11, 13, 30 });
+		//assertEquals("",tree.getRoot().toString());
+		assertEquals(3, tree.min().key.intValue());
+	}
+	@Test
+	public void testSuccessor2() {
+		Tree<Integer, Object> tree = new Tree<Integer, Object>();
+		tree.insert(new Integer[] { 10, 7, 3, 9, 20, 15, 11, 13, 30 });
+		assertEquals(11, tree.succ(tree.root).key.intValue());
+	}
+
+	@Test
+	public void testSuccessor() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(11);
+		it.insert(23);
+		it.insert(28);
+		it.insert(1);
+		assertEquals("1, 5, 11, 12, 20, 23, 28", it.toString());
+		assertEquals("((1-5-11)-12-(-20-(-23-28)))", it.toStringTree());
+		assertEquals(Integer.valueOf(11), it.successor(5).key);
+		assertEquals(Integer.valueOf(5), it.successor(1).key);
+		assertEquals(Integer.valueOf(12), it.successor(11).key);
+		assertEquals(Integer.valueOf(20), it.successor(12).key);
+		assertEquals(Integer.valueOf(23), it.successor(20).key);
+		assertEquals(Integer.valueOf(28), it.successor(23).key);
+	}
+
+	@Test
+	public void testDeleteLeaf() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.delete(20);
+		assertEquals("5, 12", it.toString());
+		Iterator<Integer> i = it.iterator();
+		assertEquals(5, i.next().intValue());
+		assertEquals(12, i.next().intValue());
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void testToStringTree1() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(3);
+		assertEquals("(1-2-3)", it.toStringTree());
+
+	}
+
+	@Test
+	public void testToStringTree2() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(5);
+		it.insert(2);
+		it.insert(1);
+		it.insert(3);
+		it.insert(7);
+		it.insert(6);
+		it.insert(8);
+		assertEquals("((1-2-3)-5-(6-7-8))", it.toStringTree());
+
+	}
+
+	@Test
+	public void testDeleteWithRightChild() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(21);
+		assertEquals("(5-12-(-20-21))", it.toStringTree());
+		it.delete(20);
+		assertEquals("(5-12-21)", it.toStringTree());
+		Iterator<Integer> i = it.iterator();
+		assertEquals(5, i.next().intValue());
+		assertEquals(12, i.next().intValue());
+		assertEquals(21, i.next().intValue());
+		assertFalse(i.hasNext());
+
+	}
+
+	@Test
+	public void testDeleteWithTwoChilds1() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(21);
+		it.insert(19);
+		assertEquals("(5-12-(19-20-21))", it.toStringTree());
+		it.delete(20);
+		assertEquals("after deletion", "(5-12-(19-21-))", it.toStringTree());
+		Iterator<Integer> i = it.iterator();
+		assertEquals(5, i.next().intValue());
+		assertEquals(12, i.next().intValue());
+		assertEquals(19, i.next().intValue());
+		assertEquals(21, i.next().intValue());
+		assertFalse(i.hasNext());
+
+	}
+
+	@Test
+	public void testDeleteWithTwoChilds2() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(21);
+		it.insert(19);
+		assertEquals("(5-12-(19-20-21))", it.toStringTree());
+		it.delete(12);
+		assertEquals("after deletion", "(5-19-(-20-21))", it.toStringTree());
+		Iterator<Integer> i = it.iterator();
+		assertEquals(5, i.next().intValue());
+		assertEquals(19, i.next().intValue());
+		assertEquals(20, i.next().intValue());
+		assertEquals(21, i.next().intValue());
+		assertFalse(i.hasNext());
+
+	}
+
+	@Test
+	public void testDeleteWithTwoChilds3() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(19);
+		it.insert(23);
+		it.insert(21);
+		it.insert(22);
+		it.insert(24);
+
+		assertEquals("(5-12-(19-20-((-21-22)-23-24)))", it.toStringTree());
+		// (5-12-(19-21-(22-23-24)))
+		it.delete(20);
+		assertEquals("after deletion", "(5-12-(19-21-(22-23-24)))",
+				it.toStringTree());
+		Iterator<Integer> i = it.iterator();
+		assertEquals(5, i.next().intValue());
+		assertEquals(12, i.next().intValue());
+		assertEquals(19, i.next().intValue());
+		assertEquals(21, i.next().intValue());
+		assertEquals(22, i.next().intValue());
+		assertEquals(23, i.next().intValue());
+		assertEquals(24, i.next().intValue());
+		assertFalse(i.hasNext());
+
+	}
+
+	@Test
+	public void testTransplant1() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(3);
+		assertEquals("(1-2-3)", it.toStringTree());
+		Tree<Integer, Object>.Node a = it.find(3);
+		Tree<Integer, Object>.Node b = it.find(1);
+		it.transplant(a, b);
+		assertEquals("(-2-1)", it.toStringTree());
+	}
+
+	@Test
+	public void testTransplant2() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(3);
+		assertEquals("(1-2-3)", it.toStringTree());
+		Tree<Integer, Object>.Node a = it.find(2);
+		Tree<Integer, Object>.Node b = it.find(1);
+		it.transplant(a, b);
+		assertEquals("1", it.toStringTree());
+	}
+
+	@Test
+	public void testReplaceRoot() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(3);
+		// given
+		assertEquals("(1-2-3)", it.toStringTree());
+		Tree<Integer, Object>.Node a = it.find(2);
+		Tree<Integer, Object>.Node b = it.new Node(10, null);
+		Tree<Integer, Object>.Node l = it.find(1);
+		Tree<Integer, Object>.Node r = it.find(3);
+		// when
+		it.replace(a, b);
+		// then
+		assertEquals("(1-10-3)", it.toStringTree());
+		assertEquals(b, it.root);
+		assertEquals(null, b.parent);
+		assertEquals(l, b.leftChild);
+		assertEquals(r, b.rightChild);
+		assertEquals(b, l.parent);
+		assertEquals(b, r.parent);
+	}
+
+	@Test
+	public void testReplaceInner() {
+
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(12);
+		it.insert(5);
+		it.insert(20);
+		it.insert(21);
+		it.insert(19);
+		assertEquals("(5-12-(19-20-21))", it.toStringTree());
+		Tree<Integer, Object>.Node a = it.find(20);
+		Tree<Integer, Object>.Node l = a.leftChild;
+		Tree<Integer, Object>.Node r = a.rightChild;
+		Tree<Integer, Object>.Node p = a.parent;
+
+		Tree<Integer, Object>.Node b = it.new Node(10, null);
+		// when
+		it.replace(a, b);
+		assertEquals("(5-12-(19-10-21))", it.toStringTree());
+
+		// then
+		assertEquals(b, p.rightChild);
+		assertEquals(p, b.parent);
+		assertEquals(l, b.leftChild);
+		assertEquals(r, b.rightChild);
+		assertEquals(b, l.parent);
+		assertEquals(b, r.parent);
+
+	}
+
+	@Test
+	public void testTransplant() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(4);
+		it.insert(3);
+		it.insert(5);
+		assertEquals("(1-2-(3-4-5))", it.toStringTree());
+		Tree<Integer, Object>.Node four = it.find(4);
+		Tree<Integer, Object>.Node one = it.find(1);
+		it.transplant(one, four);
+		assertEquals("((3-4-5)-2-)", it.toStringTree());
+	}
+
+	@Test
+	public void testIteratorSimple() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		Iterator<Integer> i = it.iterator();
+		assertTrue(i.hasNext());
+		assertEquals(2, i.next().intValue());
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void testIteratorMixed() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(2);
+		it.insert(1);
+		it.insert(4);
+		it.insert(3);
+		it.insert(5);
+		Iterator<Integer> i = it.iterator();
+		assertTrue(i.hasNext());
+		assertEquals(1, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(2, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(3, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(4, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(5, i.next().intValue());
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void testIteratorDegenerated() {
+		Tree<Integer, Object> it = new Tree<Integer, Object>();
+		it.insert(1);
+		it.insert(2);
+		it.insert(3);
+		it.insert(4);
+		it.insert(5);
+		Iterator<Integer> i = it.iterator();
+		assertTrue(i.hasNext());
+		assertEquals(1, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(2, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(3, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(4, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(5, i.next().intValue());
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void randomBSTPropertyTest() {
+		for (int j = 0; j < 100; j++) {
+			int number = (int) (Math.random() * 100) + 1;
+			SortedSet<Integer> list = new TreeSet<Integer>();
+			AVLTree<Integer, Object> it = new AVLTree<Integer, Object>();
+			for (int i = 0; i < number; i++) {
+				int randomNumber = (int) (Math.random() * 100);
+				if (list.add(randomNumber)) {
+					it.insert(randomNumber);
+				}
+			}
+			assertEquals("size", list.size(), it.size());
+			Iterator<Integer> avlTreeIterator = it.iterator();
+			for (int n : list) {
+				assertEquals("x " + n, n, avlTreeIterator.next().intValue());
+
+			}
+		}
+
+	}
+
+	@Test
+	public void testDelete() {
+		AVLTree<Integer, Object> it = new AVLTree<Integer, Object>();
+		it.insert(59);
+		it.insert(55);
+		it.insert(49);
+		it.delete(55);
+		it.delete(59);
+		assertEquals(1, it.size());
+
+	}
+
+	@Test
+	public void randomTestDeletion() {
+		for (int j = 0; j < 1000; j++) {
+			int number = j;
+			SortedSet<Integer> list = new TreeSet<Integer>();
+			AVLTree<Integer, Object> it = new AVLTree<Integer, Object>();
+			List<Integer> insertionOrder = new ArrayList<Integer>();
+			for (int i = 0; i < number; i++) {
+				int randomNumber = (int) (Math.random() * 100);
+				if (list.add(randomNumber)) {
+					insertionOrder.add(randomNumber);
+					it.insert(randomNumber);
+				}
+			}
+			List<Integer> originalInsertionOrder = new ArrayList<Integer>(
+					insertionOrder);
+
+			// now remove random elements
+			for (int i = 0; (i < (number + 1) / 2) && insertionOrder.size() > 0; i++) {
+				int randomIndex = (int) (Math.random() * insertionOrder.size());
+				int numberToRemove = insertionOrder.remove(randomIndex);
+				list.remove(numberToRemove);
+				it.delete(numberToRemove);
+
+			}
+
+			assertEquals("size \nlist:" + list + "\ntree: " + it.toStringTree()
+					+ "\nh:" + originalInsertionOrder + "\n", list.size(),
+					it.size());
+			Iterator<Integer> avlTreeIterator = it.iterator();
+			for (int n : list) {
+				assertEquals("x " + list + "\n tree: " + it.toStringTree()
+						+ "\n" + originalInsertionOrder, n, avlTreeIterator
+						.next().intValue());
+			}
+
+		}
+	}
+
+	@Test
+	public void testInsert22() {
+		Integer[] values = { 28, 20, 2, 72, 57, 9, 81, 17, 95, 23, 33, 96, 87,
+				5, 52, 45, 47, 73, 30 };
+		AVLTree<Integer, Object> it = new AVLTree<Integer, Object>();
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] == 5)
+				System.out.println(5);
+			it.insert(values[i]);
+			/*
+			 * assertTrue( "unbalanced after insertion of " + values[i] + "\n" +
+			 * it.toString(), it.isBalanced());
+			 */
+		}
+		assertTrue(it.isBalanced());
+
+	}
+
+}
